f23874cec6ecb284e6dcd577db1a8674e396acf2
VinayKumar1801 <107485549+VinayKumar1801@users.noreply.github.com>
2022-12-23 13:29:17 +0530

    chat app

---
+++ medical-app/components/dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useConversations } from '../contexts/convsersationsProvider';
+import OpenConversation from './openConversation';
+import Sidebar from './Sidebar';
+
+
+
+export default function Dashboard({ id }) {
+    const { selectedConversation } = useConversations()
+
+    return (
+        <div className="d-flex" style={{ height: '100vh' }}>
+            <Sidebar id={id} />
+            {selectedConversation && <OpenConversation />}
+        </div>
+    )
+}
---
+++ medical-app/pages/index.js
@@ -0,0 +1,30 @@
+// import { useState } from "react";
+import Dashboard from "../components/dashboard";
+import Login from "../components/login";
+import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ConversationsProvider } from "../contexts/convsersationsProvider";
+import { SocketProvider } from "../contexts/SocketProvider";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+
+export default function Home() {
+  const [id,setId]= useLocalStorage('id')
+  const dashboard=(
+    <SocketProvider id={id}>
+
+    <ContactsProvider>
+      <ConversationsProvider id={id}>
+      <Dashboard id={id}/>
+
+      </ConversationsProvider>
+    </ContactsProvider>
+    </SocketProvider>
+  )
+
+  return (
+    <>
+      {id ? dashboard  : <Login onIdSubmit={setId} />}
+   
+    </>
+  )
+}
