2aaeffe4c912f48083ac3c230366582eeb5d66bd
VinayKumar1801 <107485549+VinayKumar1801@users.noreply.github.com>
2022-12-23 13:46:31 +0530

    add

--- medical-app/contexts/socketProvider.js
+++ medical-app/contexts/socketProvider.js
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
 
     useEffect(() => {
         const newSocket = io(
-            'http://localhost:5000',
+           process.env.DBURL,
             { query: { id } }
         )
         setSocket(newSocket)
