580ca5e39cd5d0f6fac2e6dabc65b28c9d1963fe
Shreedhar Hegde <snhkelemane@gmail.com>
2022-12-23 14:12:51 +0530

    fixed import iddues

--- medical-app/components/dashboard.js
+++ medical-app/components/dashboard.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import { useConversations } from '../contexts/convsersationsProvider';
-import OpenConversation from './openConversation';
-import Sidebar from './Sidebar';
-
-
+import React from "react";
+import { useConversations } from "../contexts/convsersationsProvider";
+import OpenConversation from "./openConversation";
+import Sidebar from "./sidebar";
 
 export default function Dashboard({ id }) {
-    const { selectedConversation } = useConversations()
+  const { selectedConversation } = useConversations();
 
-    return (
-        <div className="d-flex" style={{ height: '100vh' }}>
-            <Sidebar id={id} />
-            {selectedConversation && <OpenConversation />}
-        </div>
-    )
+  return (
+    <div className="d-flex" style={{ height: "100vh" }}>
+      <Sidebar id={id} />
+      {selectedConversation && <OpenConversation />}
+    </div>
+  );
 }
--- medical-app/pages/index.js
+++ medical-app/pages/index.js
@@ -1,30 +1,22 @@
 // import { useState } from "react";
 import Dashboard from "../components/dashboard";
 import Login from "../components/login";
-import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ContactsProvider } from "../contexts/contactsProvider";
 import { ConversationsProvider } from "../contexts/convsersationsProvider";
-import { SocketProvider } from "../contexts/SocketProvider";
+import { SocketProvider } from "../contexts/socketProvider";
 import useLocalStorage from "../hooks/useLocalStorage";
 
-
 export default function Home() {
-  const [id,setId]= useLocalStorage('id')
-  const dashboard=(
+  const [id, setId] = useLocalStorage("id");
+  const dashboard = (
     <SocketProvider id={id}>
-
-    <ContactsProvider>
-      <ConversationsProvider id={id}>
-      <Dashboard id={id}/>
-
-      </ConversationsProvider>
-    </ContactsProvider>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
+      </ContactsProvider>
     </SocketProvider>
-  )
+  );
 
-  return (
-    <>
-      {id ? dashboard  : <Login onIdSubmit={setId} />}
-   
-    </>
-  )
+  return <>{id ? dashboard : <Login onIdSubmit={setId} />}</>;
 }
